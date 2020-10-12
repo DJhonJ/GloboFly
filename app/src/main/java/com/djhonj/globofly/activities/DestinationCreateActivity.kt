5ff980da21c7f1657931eeb612bd87e4ea3f6aa6
Jhon J Diaz <diazj691@gmail.com>
2020-10-11 20:06:01 -0500

    proyecto vacio

---
+++ app/src/main/java/com/djhonj/globofly/activities/DestinationCreateActivity.kt
@@ -0,0 +1,35 @@
+package activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.smartherd.globofly.R
+import helpers.SampleData
+import models.Destination
+import kotlinx.android.synthetic.main.activity_destiny_create.*
+
+class DestinationCreateActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_destiny_create)
+
+		val toolbar: Toolbar = findViewById(R.id.toolbar)
+		setSupportActionBar(toolbar)
+		val context = this
+
+		// Show the Up button in the action bar.
+		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+		btn_add.setOnClickListener {
+			val newDestination = Destination()
+			newDestination.city = et_city.text.toString()
+			newDestination.description = et_description.text.toString()
+			newDestination.country = et_country.text.toString()
+
+			// To be replaced by retrofit code
+			SampleData.addDestination(newDestination)
+            finish() // Move back to DestinationListActivity
+		}
+	}
+}
